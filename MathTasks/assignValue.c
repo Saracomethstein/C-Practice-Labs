7311f883d68a997e04b3e6c53b0e72e8bca77f5a
Saracomethstein <adel.sevastianov@yandex.ru>
2023-10-10 18:55:38 +0300

    Add Task2

---
+++ MathTasks/assignValue.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int AssingValue(int x)
+{
+    int k = 0;
+    int i = 0;
+    while (x != 0)
+    {
+        k = x % 10;
+        if (i == 2)
+        {
+            break;
+        }
+        x = x / 10;
+        ++i;
+    }
+    return k;
+}
+
+int main()
+{
+    int x = 0;
+    char scanChar = 0;
+
+    printf("Add X (integer or real number):");
+    int scan = (scanf("%d%c", &x, &scanChar));
+
+    if (scan == 2 && scanChar == '\n' && x >= 0)
+    {
+        printf("Result: %d", AssingValue(x));
+    }
+    else
+    {
+        printf("n/a");
+    }
+    return 0;
+}
