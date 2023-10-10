78264eaaf199aad3802797e876338a6737ba6d75
Saracomethstein <adel.sevastianov@yandex.ru>
2023-10-10 19:31:08 +0300

    Add Task5

---
+++ MathTasks/reverseOrder.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int ReverseOrder(int x){
+    int k = 0;
+    while (x)
+    {
+        k = k * 10 + x % 10;
+        x = x / 10;
+    }
+    return k;
+}
+
+int main(){
+    int x = 0;
+    char scanChar = 0;
+
+    printf("Add X (integer or real number):");
+    int scan = (scanf("%d%c", &x, &scanChar));
+
+    if (scan == 2 && scanChar == '\n')
+    {
+        printf("Result: %d", ReverseOrder(x));
+    }
+    else
+    {
+        printf("n/a");
+    }
+    return 0;
+}
