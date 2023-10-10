4c573348160c62d7c1cf77483d28b18544b536c5
Saracomethstein <adel.sevastianov@yandex.ru>
2023-10-10 19:03:33 +0300

    Add Task3

---
+++ MathTasks/sumOfDigits.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int SumOfDigits(int x){
+    int k = 0;
+
+    while (x != 0)
+    {
+        k += x % 10;
+        x = x / 10;
+    }
+    
+    return k;
+}
+
+int main(){
+    int x = 0;
+    char scanChar = 0;
+
+    printf("Add X (integer or real number):");
+    int scan = (scanf("%d%c", &x, &scanChar));
+
+    if (scan == 2 && scanChar == '\n' && x >= 0)
+    {
+        printf("Result: %d", SumOfDigits(x));
+    }
+    else
+    {
+        printf("n/a");
+    }
+    return 0;
+}
