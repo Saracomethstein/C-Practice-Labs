8d80857d6439717ecef36302349de0822a4e76ba
Saracomethstein <adel.sevastianov@yandex.ru>
2023-10-11 12:31:58 +0300

    Add Task10

---
+++ MathTasks/packagingUnpacking.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+unsigned int Packing(unsigned int pack)
+{
+    char charA = 'A';
+    char charB = 'B';
+    char charC = 'C';
+    char charD = 'D';
+
+    pack = pack | (charA << 24);
+    pack = pack | (charB << 16);
+    pack = pack | (charC << 8);
+    pack = pack | charD;
+
+    return pack;
+}
+
+void Unpacking(unsigned int pack)
+{
+    char extCharA = (char)((pack >> 24) & 0xFF);
+    char extCharB = (char)((pack >> 16) & 0xFF);
+    char extCharC = (char)((pack >> 8) & 0xFF);
+    char extCharD = (char)(pack & 0xFF);
+
+    printf("Unpacking.\nCharA = %c\nCharB = %c\nCharC = %c\nCharD = %c\n\n", extCharA, extCharB, extCharC, extCharD);
+}
+
+int main()
+{
+    unsigned int pack = 0;
+    printf("\nPacking.\nResult: %d\n\n", Packing(pack));
+    Unpacking(Packing(pack));
+    return 0;
+}
