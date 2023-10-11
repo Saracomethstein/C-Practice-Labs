0e4afcd2df474b625d082eb07b41d76fbc11fde3
Saracomethstein <adel.sevastianov@yandex.ru>
2023-10-11 12:49:38 +0300

    Add Task11

---
+++ MathTasks/bitReplacement.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+unsigned int BitReplacement(){
+    int p = 3;
+    int n = 2;
+    unsigned int x = 150; // 150
+    unsigned int y = 20;  // 20
+
+    unsigned int mask = ~(~0u << n) << (p - n + 1);
+    y = ~y;
+    x = x & ~mask;
+    x = x | (y << (p - n + 1));
+
+    return x;
+}
+
+int main(){
+    printf("Result: %u\n", BitReplacement());
+    return 0;
+}
